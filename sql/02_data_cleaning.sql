b6fe85ff67acb2f2a3956db2b840476f593bb80d
Mahima Kaur <97355518+KaurMahima@users.noreply.github.com>
2026-01-08 12:42:06 -0500

    add queries for data cleaning operations : rename variables, proper case names, deduplicate the dataset

---
+++ sql/02_data_cleaning.sql
@@ -0,0 +1,100 @@
+-- =====================================================================
+-- 02_DATA_CLEANING.SQL
+-- =====================================================================
+
+-- Create backup data before any modifications 
+CREATE TABLE IF NOT EXISTS healthcare_data_backup AS
+SELECT * FROM healthcare_data;
+
+PRAGMA table_info(healthcare_data_backup);
+SELECT count(*) as backup_row_count FROM healthcare_data_backup;
+
+-- critical issue found:
+-- improper name casing 
+-- white spaces in the variables name 
+-- duplicate patient record 
+-- the dataset does not contain patient id 
+-- negative billing amount (108 records)
+-- add event/visit id information 
+
+-- ============================================
+-- STEP 2: DATA CLEANING OPERATIONS
+-- ============================================
+
+-- Rename all columns to snake_case
+ALTER TABLE healthcare_data RENAME COLUMN "Name" TO name;
+ALTER TABLE healthcare_data RENAME COLUMN "Age" TO age;
+ALTER TABLE healthcare_data RENAME COLUMN "Gender" TO gender;
+ALTER TABLE healthcare_data RENAME COLUMN "Blood Type" TO blood_type;
+ALTER TABLE healthcare_data RENAME COLUMN "Medical Condition" TO medical_condition;
+ALTER TABLE healthcare_data RENAME COLUMN "Date of Admission" TO date_of_admission;
+ALTER TABLE healthcare_data RENAME COLUMN "Doctor" TO doctor;
+ALTER TABLE healthcare_data RENAME COLUMN "Hospital" TO hospital;
+ALTER TABLE healthcare_data RENAME COLUMN "Insurance Provider" TO insurance_provider;
+ALTER TABLE healthcare_data RENAME COLUMN "Billing Amount" TO billing_amount;
+ALTER TABLE healthcare_data RENAME COLUMN "Room Number" TO room_number;
+ALTER TABLE healthcare_data RENAME COLUMN "Admission Type" TO admission_type;
+ALTER TABLE healthcare_data RENAME COLUMN "Discharge Date" TO discharge_date;
+ALTER TABLE healthcare_data RENAME COLUMN "Medication" TO medication;
+ALTER TABLE healthcare_data RENAME COLUMN "Test Results" TO test_results;
+
+-- Trim white spaces if any 
+UPDATE healthcare_data
+SET name = TRIM(name),
+    gender = TRIM(gender),
+    blood_type = TRIM(blood_type),
+    medical_condition = TRIM(medical_condition),
+    doctor = TRIM(doctor),
+    hospital = TRIM(hospital),
+    insurance_provider = TRIM(insurance_provider),
+    admission_type = TRIM(admission_type),
+    medication = TRIM(medication),
+    test_results = TRIM(test_results);
+
+-- Update the name column to Proper case (just the first given name in caps)
+UPDATE healthcare_data 
+SET name = UPPER(LEFT(name, 1)) || LOWER(SUBSTRING("Name", 2));
+
+-- Validate the proper name casing cleaning query 
+SELECT name
+FROM healthcare_data 
+LIMIT 10;
+
+-- Duplicates count check 
+SELECT COUNT(*) AS dup_count
+FROM (
+        SELECT COUNT(*) as cnt
+        FROM healthcare_data 
+        GROUP BY name, age, gender, blood_type, medical_condition, date_of_admission,
+    doctor, hospital, insurance_provider, billing_amount, room_number,
+    admission_type, discharge_date, medication, test_results
+    HAVING COUNT(*) > 1) d ;
+
+-- Another query to check the duplicate rows 
+WITh total AS (
+        SELECT count(*) as total_rows FROM healthcare_data
+),
+distincts AS (
+        SELECT count(*) as distinct_rows FROM (SELECT distinct * FROM healthcare_data) t
+)
+SELECT total_rows, distinct_rows, total_rows - distinct_rows as  duplicate_rows 
+FROM total, distincts; 
+
+-- Drop duplciate rows 
+
+BEGIN TRANSACTION;
+
+CREATE TABLE healthcare_data_dedup AS 
+SELECT DISTINCT *       
+FROM healthcare_data;
+
+DROP TABLE IF EXISTS healthcare_data;
+ALTER TABLE healthcare_data_dedup RENAME TO healthcare_data;
+
+COMMIT;
+
+
+
+
+
+
